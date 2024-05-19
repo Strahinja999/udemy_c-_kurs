4481fa4f855d5f574b556ac33337878db75fe0b5
METERANDCONTROL\strahinja.milosevic <strahinja.milosevic@meterandcontrol.com>
2024-05-19 13:33:39 +0200

    Completed ass1 - simple calculator

--- CSharp_kurs/simple_calcualtor_ass1/Program.cs
+++ CSharp_kurs/simple_calcualtor_ass1/Program.cs
@@ -1,3 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("FirstLine");
+﻿Console.WriteLine("Hello!\r\nInput the first number:\r");
+string firstInput  = Console.ReadLine();
+int firstNumber = int.Parse(firstInput);
+Console.WriteLine("Input the second number:");
+string secondInput = Console.ReadLine();
+int secondNumber = int.Parse(secondInput);
+Console.WriteLine("What do you want to do with those numbers?\n[A]dd\n[S]ubtract\n[M]ultiply");
+string userChoise = Console.ReadLine();
+if(userChoise.ToLower() == "a")
+{
+    Console.WriteLine(firstNumber + " + " +  secondNumber + " = " + Add(firstNumber, secondNumber));
+}else if (userChoise.ToLower() == "s")
+{
+    Console.WriteLine(firstNumber + " - " + secondNumber + " = " + Sub(firstNumber, secondNumber));
+}
+else if (userChoise == "m")
+{
+    Console.WriteLine(firstNumber + " * " + secondNumber + " = " + Mul(firstNumber, secondNumber));
+}
+else
+{
+    Console.WriteLine("Invalid input");
+}
+Console.WriteLine("Press any key to close");
+Console.ReadKey();
+
+int Add(int n1, int n2)
+{
+    return n1 + n2;
+}
+int Sub(int n1, int n2)
+{
+    return n1 - n2;
+}
+int Mul(int n1, int n2)
+{
+    return n1 * n2;
+}
