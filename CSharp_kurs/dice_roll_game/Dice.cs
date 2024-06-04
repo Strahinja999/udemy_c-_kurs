2e9c244d22b0673b47c05a8b737504a394f5eb27
METERANDCONTROL\strahinja.milosevic <strahinja.milosevic@meterandcontrol.com>
2024-06-04 15:27:58 +0200

    My first solution

---
+++ CSharp_kurs/dice_roll_game/Dice.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dice_roll_game;
+public class Dice
+{
+    public int Number { get; }
+    public Dice()
+    {
+        Number = new Random().Next(1,6);
+    }
+}
---
+++ CSharp_kurs/dice_roll_game/Program.cs
@@ -0,0 +1,23 @@
+﻿using dice_roll_game;
+
+Dice _dice = new Dice();
+//Console.WriteLine(_dice.Number);
+Console.WriteLine("Dice rolled. Guess what number it shows in 3 tries.");
+
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine("Enter your guest: ");
+    string userInput = Console.ReadLine();
+    int userNumber = int.Parse(userInput);
+    if (userNumber == _dice.Number)
+    {
+        Console.WriteLine("You win");
+        return;
+    }
+    else
+    {
+        Console.WriteLine("Wrong number");
+    }
+}
+Console.WriteLine("You lose");
+
